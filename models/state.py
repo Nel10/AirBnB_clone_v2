ae30edb6c75cdd09543ec224270e75e3e6544954
Nel10 <3461@holbertonschool.com>
2021-12-22 10:46:55 -0500

    Update task 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import relationship
+from sqlalchemy import Column, String, Integer, ForeignKey
+from models import storage
+from models.city import City
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City')
+    else:
+        name = ''
+        cities = storage.all(City)
