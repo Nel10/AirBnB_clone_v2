521af146b490158f86c62044fbc5fb417f5be5b4
jdbalantar <78030554+jdbalantar@users.noreply.github.com>
2021-12-22 22:09:49 -0500

    Add files via upload

--- models/city.py
+++ models/city.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from os import getenv, name
+from sqlalchemy.sql.schema import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import String
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import name
-import sqlalchemy
 
-
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place')
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    class City(BaseModel, Base):
+        """ The city class, contains state ID and name """
+        _tablename_ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place")
+else:
+    class City:
+        '''Class City for FileStorage'''
+        name = ""
+        state_id = ""
