16c6418e7834b235bcb309e853a4ec842fa7353d
jdbalantar <78030554+jdbalantar@users.noreply.github.com>
2021-12-22 21:45:09 -0500

    Fixing errors

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from os import getenv
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import String
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+import models
 
 
-class Amenity(BaseModel):
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    class Amenity(BaseModel, Base):
+        _tablename_ = "amenities"
+        name = Column(String(128), nullable=False)
+else:
     name = ""
