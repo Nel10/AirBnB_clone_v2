4d255b64bc0189dde6483856be2ce90bc76fc1ec
Nel10 <3461@holbertonschool.com>
2022-01-13 01:24:41 -0500

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,8 +12,10 @@ echo "
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee -a /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "52i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sudo chown -hR ubuntu:ubuntu /data/
+str="\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+sudo sed -i "24i $str" /etc/nginx/sites-available/default
+sudo service nginx reload
 sudo service nginx start
