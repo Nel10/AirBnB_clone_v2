61d8d7bca8676da24ef8de207c084ce45bbd6c72
Nel10 <3461@holbertonschool.com>
2022-01-13 21:33:54 -0500

    file puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,6 +1,6 @@
-# setup web static
+# setup we_static
 
-exec { 'configure everything':
-    command  => 'sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y install nginx && sudo ufw allow 'Nginx HTTP' && sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/ && echo "<html><head></head><body>Holberton School</body></html>" | sudo tee -a /data/web_static/releases/test/index.html > /dev/null && sudo ln -sf /data/web_static/releases/test/ /data/web_static/current && sudo chown -hR ubuntu:ubuntu /data/ && str="\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" && sudo sed -i "24i $str" /etc/nginx/sites-available/default && sudo service nginx reload && sudo service nginx start',
-    provider => shell,
+exec { 'configuring':
+  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo service nginx start && sudo mkdir -p /data/web_static/shared/ && sudo mkdir -p /data/web_static/releases/test/ && echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null && sudo ln -sf /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu:ubuntu /data/ && sudo sed -i \'24i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\' /etc/nginx/sites-available/default && sudo service nginx restart',
+  provider => shell,
 }
